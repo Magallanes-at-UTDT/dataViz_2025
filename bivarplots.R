815724331f1664a818ca1a40cf2ff05854d5c2a8
profemagallanes-pucp <jmagallanes@pucp.edu.pe>
2025-08-13 00:35:53 -0300

    Update bivarplots.R

--- bivarplots.R
+++ bivarplots.R
@@ -1,36 +1,37 @@
-link="https://github.com/Magallanes-at-UTDT/dataViz_2025/raw/refs/heads/main/fragilityVariabilityPerformance_2023_2006.csv"
+rm(list = ls())
+link="https://github.com/Magallanes-at-UTDT/dataViz_2025/raw/refs/heads/main/fragilityVariabilityGrowth_2023_2006.csv"
 
-varperf=read.csv(link)
-head(varperf)
+vargrth=read.csv(link)
+head(vargrth)
 
 library(ggplot2)
 
-ggplot(data=varperf) + geom_point(aes(x=p3_human_rights_performance,y=p3_human_rights_variability))
+ggplot(data=vargrth) + geom_point(aes(x=p3_human_rights_growth,y=p3_human_rights_variability))
 
-ggplot(data=varperf) + geom_point(aes(x=p3_human_rights_performance,y=p3_human_rights_variability,color=region))
+ggplot(data=vargrth) + geom_point(aes(x=p3_human_rights_growth,y=p3_human_rights_variability,color=region))
 
-ggplot(data=varperf) + geom_point(aes(x=p3_human_rights_performance,y=p3_human_rights_variability)) + facet_wrap(~region)
+ggplot(data=vargrth) + geom_point(aes(x=p3_human_rights_growth,y=p3_human_rights_variability)) + facet_wrap(~region)
 
-table(varperf$region)
+table(vargrth$region)
 
 americas=c('South America','Central America and the Caribbean','North America')
-ggplot(data=varperf[varperf$region %in% americas,],aes(x=p3_human_rights_performance,y=p3_human_rights_variability)) +
+ggplot(data=vargrth[vargrth$region %in% americas,],aes(x=p3_human_rights_growth,y=p3_human_rights_variability)) +
     geom_text(aes(label=country,color=region)) + theme(legend.position = "bottom")
 
-ggplot(data=varperf[varperf$region %in% americas,],aes(x=p3_human_rights_performance,y=p3_human_rights_variability)) +
+ggplot(data=vargrth[vargrth$region %in% americas,],aes(x=p3_human_rights_growth,y=p3_human_rights_variability)) +
     geom_text(aes(label=country),size=2) + facet_grid(region~.)
 
 # install.packages("ggrepel")
 library(ggrepel)
 
 americas=c('South America','Central America and the Caribbean','North America')
-bivar_americas=ggplot(data=varperf[varperf$region %in% americas,],aes(x=p3_human_rights_performance,y=p3_human_rights_variability)) + theme_light()+
+bivar_americas=ggplot(data=vargrth[vargrth$region %in% americas,],aes(x=p3_human_rights_growth,y=p3_human_rights_variability)) + theme_light()+
     geom_label_repel(aes(label=country,color=region)) + theme(legend.position = "bottom") +
     geom_vline(xintercept = 0, color='grey') + scale_y_log10()
 bivar_americas
 
 farlands=c('South America','East and Southeast Asia')
-bivar_farlands=ggplot(data=varperf[varperf$region %in% farlands,],aes(x=p3_human_rights_performance,y=p3_human_rights_variability)) + theme_light()+
+bivar_farlands=ggplot(data=vargrth[vargrth$region %in% farlands,],aes(x=p3_human_rights_growth,y=p3_human_rights_variability)) + theme_light()+
     geom_label_repel(aes(label=country,color=region,size=2),max.overlaps=20) + theme(legend.position = "bottom") +
     geom_vline(xintercept = 0, color='grey') + scale_y_log10()
 
